91fe0fae7dc26480e74da9c6e9b352ee634596d1
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2025-07-20 18:18:48 +0900

    feat: 로그인 후 사용자의 정보를 db에 저장하기

---
+++ src/actions/user.action.ts
@@ -0,0 +1,30 @@
+"use server";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export async function syncUser(){
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) return;
+
+    const existingUser = await prisma.user.findUnique({ //사용자 존재하는지 체크
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    if (existingUser) return existingUser;
+
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+
+    return dbUser;
+}
